3e46920862c7113f3c548ee915d7b06571977584
renanfelipelopes <renanlopes.ads@gmail.com>
2022-02-28 21:35:15 -0300

    AuthController JWT

---
+++ src/DevIO.Api/Configuration/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevIO.Api.Controllers;
+
+namespace DevIO.Api.Configuration
+{
+    public class AuthController : MainController
+    {
+    }
+}
