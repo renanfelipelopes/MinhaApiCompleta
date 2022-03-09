59da8c781d064d90aef05b952c2db4c404cbbc18
renanfelipelopes <renanlopes.ads@gmail.com>
2022-03-08 22:33:43 -0300

    CustomAuthorization

---
+++ src/DevIO.Api/Extensions/CustomAuthorize.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Extensions
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+        {
+            return context.User.Identity.IsAuthenticated &&
+                    context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+        }
+    }
+
+    public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+    {
+        public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(type: typeof(RequisitoClaimFilter))
+        {
+            Arguments = new object[] { new Claim(type: claimName, claimValue) };
+        }
+    }
+    
+}
+        
