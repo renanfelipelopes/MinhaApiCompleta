4a4c6a60ca52e27d10164e4d6248fee2223c8f59
renanfelipelopes <renanlopes.ads@gmail.com>
2022-03-08 22:37:07 -0300

    CustomAuthorization

--- src/DevIO.Api/Extensions/CustomAuthorize.cs
+++ src/DevIO.Api/Extensions/CustomAuthorize.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace DevIO.Api.Extensions
 {
@@ -24,6 +22,29 @@ namespace DevIO.Api.Extensions
             Arguments = new object[] { new Claim(type: claimName, claimValue) };
         }
     }
-    
+
+    public class RequisitoClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequisitoClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new StatusCodeResult(statusCode: 401);
+                return;
+            }
+
+            if (!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new StatusCodeResult(403);
+            }
+        }
+    }    
 }
         
