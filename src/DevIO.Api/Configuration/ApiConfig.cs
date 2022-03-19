68a9ae595e55e60ecc2acdcec373ad30be45f614
renanfelipelopes <renanlopes.ads@gmail.com>
2022-03-19 16:33:33 -0300

    Versionando a API

--- src/DevIO.Api/Configuration/ApiConfig.cs
+++ src/DevIO.Api/Configuration/ApiConfig.cs
@@ -15,6 +15,19 @@ namespace DevIO.Api.Configuration
     {
         public static IServiceCollection WebApiConfig(this IServiceCollection services)
         {
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
