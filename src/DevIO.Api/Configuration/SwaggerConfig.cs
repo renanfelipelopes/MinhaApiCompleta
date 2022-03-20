c1219c60ef3db597f9fb38f67d4d97b548207653
renanfelipelopes <renanlopes.ads@gmail.com>
2022-03-20 15:55:26 -0300

    SwaggerAuthorizedMiddleware

--- src/DevIO.Api/Configuration/SwaggerConfig.cs
+++ src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -98,4 +98,65 @@ namespace DevIO.Api.Configuration
             return info;
         }
     }
+
+    public class SwaggerDefaultValues : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+            {
+                return;
+            }
+
+            foreach (var parameter in operation.Parameters)
+            {
+                var description = context.ApiDescription
+                    .ParameterDescriptions
+                    .First(p => p.Name == parameter.Name);
+
+                var routeInfo = description.RouteInfo;
+
+                operation.Deprecated = OpenApiOperation.DeprecatedDefault;
+
+                if (parameter.Description == null)
+                {
+                    parameter.Description = description.ModelMetadata?.Description;
+                }
+
+                if (routeInfo == null)
+                {
+                    continue;
+                }
+
+                if (parameter.In != ParameterLocation.Path && parameter.Schema.Default == null)
+                {
+                    parameter.Schema.Default = new OpenApiString(routeInfo.DefaultValue.ToString());
+                }
+
+                parameter.Required |= !routeInfo.IsOptional;
+            }
+        }
+    }
+
+    public class SwaggerAuthorizedMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SwaggerAuthorizedMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/swagger")
+                && !context.User.Identity.IsAuthenticated)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return;
+            }
+
+            await _next.Invoke(context);
+        }
+    }
 }
