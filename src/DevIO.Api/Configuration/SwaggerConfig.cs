d791996d4b3b96b34c4422d5fcd468ac6e9b96a5
renanfelipelopes <renanlopes.ads@gmail.com>
2022-03-20 15:54:35 -0300

    ConfigureSwaggerOptions

---
+++ src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace DevIO.Api.Configuration
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+
+        static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "API - desenvolvedor.io",
+                Version = description.ApiVersion.ToString(),
+                Description = "Esta API faz parte do curso REST com ASP.NET Core WebAPI.",
+                Contact = new OpenApiContact() { Name = "Eduardo Pires", Email = "contato@desenvolvedor.io" },
+                License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " Esta versão está obsoleta!";
+            }
+
+            return info;
+        }
+    }
+}
